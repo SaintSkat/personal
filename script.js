2ea913f8ca264f4169cf021cd8e54dc38be9a732
Korulina Lera <korulina.vd@students.dvfu.ru>
2023-10-25 01:44:51 +1000

    Add juicy video

--- script.js
+++ script.js
@@ -9,4 +9,21 @@ window.onload = () => {
         scale: 1.00,
         scaleMobile: 1.00
     });
+
+    let video = document.getElementById('video');
+    let listener = () => {
+        video.play();
+    };
+    document.getElementById('starter').addEventListener('click', () => {
+        video.classList.add('play');
+        video.play();
+        document.body.classList.add('waiting');
+        video.addEventListener('pause', listener);
+    });
+    video.addEventListener('ended', () => {
+        video.classList.remove('play');
+        document.body.classList.remove('waiting');
+        video.removeEventListener('pause', listener);
+        video.pause();
+    });
 };
